e635d4c2a0e1c22093fc5a21d2b81df2cbd56863
Daniel Ruoso <daniel@ruoso.com>
2010-08-27 11:02:36 -0300

    não precisa mais dos cruds especificos

--- lib/Projeto/Controller/TiposObjetivo.pm
+++
@@ -1,9 +0,0 @@
-package Projeto::Controller::TiposObjetivo;
-use Projeto::Form::TipoObjetivo;
-use strict;
-use warnings;
-use base "Catalyst::Controller";
-
-
-1;
-
--- lib/Projeto/Controller/TiposRestricao.pm
+++
@@ -1,9 +0,0 @@
-package Projeto::Controller::TiposRestricao;
-use Projeto::Form::TipoRestricao;
-use strict;
-use warnings;
-use base "Catalyst::Controller";
-
-
-1;
-
